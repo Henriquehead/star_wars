06beabc3b6df41e62b3c330efbc1dfc54f480206
Henriquehead <carloshenriquegastronomia@gmail.com>
2024-05-31 04:14:47 -0300

    feature

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console .log('ok')
+let currentPageUrl = 'https://swapi.dev/api/people/';
+
+window.onload = () => {
+    try {
+        loadCharacters(currentPageUrl);
+    }catch (error) {
+        console.log(error);
+    }
+}
