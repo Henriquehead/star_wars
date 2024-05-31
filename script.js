f604db522c79fbcdbb2d3daf7c7c0f3e94646013
Henriquehead <carloshenriquegastronomia@gmail.com>
2024-05-31 05:23:14 -0300

    features

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let currentPageUrl = 'https://swapi.dev/api/people/';
 
-window.onload = () => {
+window.onload = async () => {   // Ativa a função toda vez que a pg é carregada
     try {
-        loadCharacters(currentPageUrl);
+        await loadCharacters(currentPageUrl);
     }catch (error) {
         console.log(error);
+        alert("erro ao carregar partes")
+    }
+}
+
+async function loadCharacters(url) {
+    const mainContent = document.getElementById("main-content");
+    mainContent.innerHTML = ""; //Limpar os resultados anteriores
+
+    try {
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((character) => {  //Criação dos cards
+            const card = document.createElement("div");
+            card.style.backgroundImage = `url ("https://starwars-visualguide.com/assets/img/characters/2.jpg")`;
+            card.className = 'cards'
+        })
+        console.log(response)
+
+    }catch(error) {
+        alert("Erro ao carregar os cards");
+        console.log(error)
     }
 }
